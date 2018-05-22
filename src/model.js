a1fd8bdc872a5c6300b2c3b7bf50e7a49172c2a5
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-05-23 01:00:22 +0200

    convert 4 space tab to 2 space of the rest

--- src/model.js
+++ src/model.js
@@ -7,82 +7,82 @@ import {solvePolynomialRegression} from './models/polynomial-regression';
 import * as ui from './ui';
 
 async function singleUnitExample() {
-    // Define a model for linear regression.
-    const model = tf.sequential();
-    model.add(tf.layers.dense({units: 1, inputShape: [1]}));
+  // Define a model for linear regression.
+  const model = tf.sequential();
+  model.add(tf.layers.dense({units: 1, inputShape: [1]}));
 
-    // Prepare the model for training: Specify the loss and the optimizer.
-    model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
+  // Prepare the model for training: Specify the loss and the optimizer.
+  model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
 
-    // Generate some synthetic data for training.
-    const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
-    const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
+  // Generate some synthetic data for training.
+  const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
+  const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
 
-    // Train the model using the data.
-    await model.fit(xs, ys);
+  // Train the model using the data.
+  await model.fit(xs, ys);
 
-    // Use the model to do inference on a data point the model hasn't seen before:
-    model.predict(tf.tensor2d([5], [1, 1])).print();
+  // Use the model to do inference on a data point the model hasn't seen before:
+  model.predict(tf.tensor2d([5], [1, 1])).print();
 }
 
 // singleUnitExample();
 
 async function dynamicGraphic() {
-    // wait until DOM will be rendered
-    await new Promise((resolve) => setTimeout(resolve, 100));
+  // wait until DOM will be rendered
+  await new Promise((resolve) => setTimeout(resolve, 100));
 
-    // draw graphic
-    const parentEl = document.getElementById('graphics');
+  // draw graphic
+  const parentEl = document.getElementById('graphics');
 
-    var vlSpec = {
-        '$schema': 'https://vega.github.io/schema/vega-lite/v2.json',
-        'data': {'name': 'table'},
-        'width': 400,
-        'mark': 'line',
-        'encoding': {
-            'x': {'field': 'x', 'type': 'quantitative', 'scale': {'zero': false}},
-            'y': {'field': 'y', 'type': 'quantitative'},
-            'color': {'field': 'category', 'type': 'nominal'}
-        }
-    };
-
-    const res = await vegaEmbed(parentEl, vlSpec, {
-        actions: false,
-    });
-
-    /**
-     * Generates a new tuple with random walk.
-     */
-    function seriesGenerator() {
-        var counter = -1;
-        var previousY = [0, 0, 0, 0];
-        return () => {
-            counter++;
-            var newVals = previousY.map((v, c) => ({
-                x: counter,
-                y: v + Math.random() * c - c / 2,
-                category: c
-            }));
-            previousY = newVals.map((v) => v.y);
-            return newVals;
-        };
+  var vlSpec = {
+    '$schema': 'https://vega.github.io/schema/vega-lite/v2.json',
+    'data': {'name': 'table'},
+    'width': 400,
+    'mark': 'line',
+    'encoding': {
+      'x': {'field': 'x', 'type': 'quantitative', 'scale': {'zero': false}},
+      'y': {'field': 'y', 'type': 'quantitative'},
+      'color': {'field': 'category', 'type': 'nominal'}
     }
+  };
 
-    // generate data on-fly
-    var valueGenerator = seriesGenerator();
-    var minimumX = -100;
+  const res = await vegaEmbed(parentEl, vlSpec, {
+    actions: false,
+  });
 
-    window.setInterval(() => {
-        minimumX++;
+  /**
+   * Generates a new tuple with random walk.
+   */
+  function seriesGenerator() {
+    var counter = -1;
+    var previousY = [0, 0, 0, 0];
+    return () => {
+      counter++;
+      var newVals = previousY.map((v, c) => ({
+        x: counter,
+        y: v + Math.random() * c - c / 2,
+        category: c
+      }));
+      previousY = newVals.map((v) => v.y);
+      return newVals;
+    };
+  }
 
-        const changeSet = vega.changeset()
-            .insert(valueGenerator())
-            .remove((t) => t.x < minimumX);
+  // generate data on-fly
+  var valueGenerator = seriesGenerator();
+  var minimumX = -100;
 
-        res.view
-            .change('table', changeSet)
-            .run();
-    }, 1000 / 30);
+  window.setInterval(() => {
+    minimumX++;
+
+    const changeSet = vega.changeset()
+      .insert(valueGenerator())
+      .remove((t) => t.x < minimumX);
+
+    res.view
+      .change('table', changeSet)
+      .run();
+  }, 1000 / 30);
 }
 
 // dynamicGraphic();
--- src/ui.js
+++ src/ui.js
@@ -18,158 +18,158 @@
 import renderChart from 'vega-embed';
 
 export async function plotData(container, xs, ys) {
-    const xvals = xs.data ? await xs.data() : xs;
-    const yvals = ys.data ? await ys.data() : ys;
+  const xvals = xs.data ? await xs.data() : xs;
+  const yvals = ys.data ? await ys.data() : ys;
 
-    const values = Array.from(yvals).map((y, i) => {
-        return {'x': xvals[i], 'y': yvals[i]};
-    });
+  const values = Array.from(yvals).map((y, i) => {
+    return {'x': xvals[i], 'y': yvals[i]};
+  });
 
-    const spec1 = {
-        // "$schema": "https://vega.github.io/schema/vega/v4.json",
-        '$schema': 'https://vega.github.io/schema/vega-lite/v2.json',
-        'width': 300,
-        'height': 300,
-        'data': {name: 'table', values},
-        'mark': 'point',
-        // 'mark': 'line',
-        'encoding': {
-            'x': {'field': 'x', 'type': 'quantitative'},
-            'y': {'field': 'y', 'type': 'quantitative'},
-        },
-    };
+  const spec1 = {
+    // "$schema": "https://vega.github.io/schema/vega/v4.json",
+    '$schema': 'https://vega.github.io/schema/vega-lite/v2.json',
+    'width': 300,
+    'height': 300,
+    'data': {name: 'table', values},
+    'mark': 'point',
+    // 'mark': 'line',
+    'encoding': {
+      'x': {'field': 'x', 'type': 'quantitative'},
+      'y': {'field': 'y', 'type': 'quantitative'},
+    },
+  };
 
-    const spec2 = {
-        "$schema": "https://vega.github.io/schema/vega/v4.json",
-        "width": 400,
-        "height": 200,
-        "padding": 5,
+  const spec2 = {
+    "$schema": "https://vega.github.io/schema/vega/v4.json",
+    "width": 400,
+    "height": 200,
+    "padding": 5,
 
-        "data": [
-            {
-                "name": "table",
-                "values": [
-                    {"category": "A", "amount": 28},
-                    {"category": "B", "amount": 55},
-                    {"category": "C", "amount": 43},
-                    {"category": "D", "amount": 91},
-                    {"category": "E", "amount": 81},
-                    {"category": "F", "amount": 53},
-                    {"category": "G", "amount": 19},
-                    {"category": "H", "amount": 87}
-                ]
-            }
-        ],
-
-        "signals": [
-            {
-                "name": "tooltip",
-                "value": {},
-                "on": [
-                    {"events": "rect:mouseover", "update": "datum"},
-                    {"events": "rect:mouseout", "update": "{}"}
-                ]
-            }
-        ],
-
-        "scales": [
-            {
-                "name": "xscale",
-                "type": "band",
-                "domain": {"data": "table", "field": "category"},
-                "range": "width",
-                "padding": 0.05,
-                "round": true
-            },
-            {
-                "name": "yscale",
-                "domain": {"data": "table", "field": "amount"},
-                "nice": true,
-                "range": "height"
-            }
-        ],
-
-        "axes": [
-            {"orient": "bottom", "scale": "xscale"},
-            {"orient": "left", "scale": "yscale"}
-        ],
-
-        "marks": [
-            {
-                "type": "rect",
-                "from": {"data": "table"},
-                "encode": {
-                    "enter": {
-                        "x": {"scale": "xscale", "field": "category"},
-                        "width": {"scale": "xscale", "band": 1},
-                        "y": {"scale": "yscale", "field": "amount"},
-                        "y2": {"scale": "yscale", "value": 0}
-                    },
-                    "update": {
-                        "fill": {"value": "steelblue"}
-                    },
-                    "hover": {
-                        "fill": {"value": "red"}
-                    }
-                }
-            },
-            {
-                "type": "text",
-                "encode": {
-                    "enter": {
-                        "align": {"value": "center"},
-                        "baseline": {"value": "bottom"},
-                        "fill": {"value": "#333"}
-                    },
-                    "update": {
-                        "x": {"scale": "xscale", "signal": "tooltip.category", "band": 0.5},
-                        "y": {"scale": "yscale", "signal": "tooltip.amount", "offset": -2},
-                        "text": {"signal": "tooltip.amount"},
-                        "fillOpacity": [
-                            {"test": "datum === tooltip", "value": 0},
-                            {"value": 1}
-                        ]
-                    }
-                }
-            }
+    "data": [
+      {
+        "name": "table",
+        "values": [
+          {"category": "A", "amount": 28},
+          {"category": "B", "amount": 55},
+          {"category": "C", "amount": 43},
+          {"category": "D", "amount": 91},
+          {"category": "E", "amount": 81},
+          {"category": "F", "amount": 53},
+          {"category": "G", "amount": 19},
+          {"category": "H", "amount": 87}
         ]
-    };
+      }
+    ],
 
-    return renderChart(container, spec2, {actions: false});
+    "signals": [
+      {
+        "name": "tooltip",
+        "value": {},
+        "on": [
+          {"events": "rect:mouseover", "update": "datum"},
+          {"events": "rect:mouseout", "update": "{}"}
+        ]
+      }
+    ],
+
+    "scales": [
+      {
+        "name": "xscale",
+        "type": "band",
+        "domain": {"data": "table", "field": "category"},
+        "range": "width",
+        "padding": 0.05,
+        "round": true
+      },
+      {
+        "name": "yscale",
+        "domain": {"data": "table", "field": "amount"},
+        "nice": true,
+        "range": "height"
+      }
+    ],
+
+    "axes": [
+      {"orient": "bottom", "scale": "xscale"},
+      {"orient": "left", "scale": "yscale"}
+    ],
+
+    "marks": [
+      {
+        "type": "rect",
+        "from": {"data": "table"},
+        "encode": {
+          "enter": {
+            "x": {"scale": "xscale", "field": "category"},
+            "width": {"scale": "xscale", "band": 1},
+            "y": {"scale": "yscale", "field": "amount"},
+            "y2": {"scale": "yscale", "value": 0}
+          },
+          "update": {
+            "fill": {"value": "steelblue"}
+          },
+          "hover": {
+            "fill": {"value": "red"}
+          }
+        }
+      },
+      {
+        "type": "text",
+        "encode": {
+          "enter": {
+            "align": {"value": "center"},
+            "baseline": {"value": "bottom"},
+            "fill": {"value": "#333"}
+          },
+          "update": {
+            "x": {"scale": "xscale", "signal": "tooltip.category", "band": 0.5},
+            "y": {"scale": "yscale", "signal": "tooltip.amount", "offset": -2},
+            "text": {"signal": "tooltip.amount"},
+            "fillOpacity": [
+              {"test": "datum === tooltip", "value": 0},
+              {"value": 1}
+            ]
+          }
+        }
+      }
+    ]
+  };
+
+  return renderChart(container, spec2, {actions: false});
 }
 
 export async function plotDataAndPredictions(container, xs, ys, preds) {
-    const xvals = await xs.data();
-    const yvals = await ys.data();
-    const predVals = await preds.data();
+  const xvals = await xs.data();
+  const yvals = await ys.data();
+  const predVals = await preds.data();
 
-    const values = Array.from(yvals).map((y, i) => {
-        return {'x': xvals[i], 'y': yvals[i], pred: predVals[i]};
-    });
+  const values = Array.from(yvals).map((y, i) => {
+    return {'x': xvals[i], 'y': yvals[i], pred: predVals[i]};
+  });
 
-    const spec = {
-        '$schema': 'https://vega.github.io/schema/vega-lite/v2.json',
-        'width': 300,
-        'height': 300,
-        'data': {'values': values},
-        'layer': [
-            {
-                'mark': 'point',
-                'encoding': {
-                    'x': {'field': 'x', 'type': 'quantitative'},
-                    'y': {'field': 'y', 'type': 'quantitative'}
-                }
-            },
-            {
-                'mark': 'line',
-                'encoding': {
-                    'x': {'field': 'x', 'type': 'quantitative'},
-                    'y': {'field': 'pred', 'type': 'quantitative'},
-                    'color': {'value': 'tomato'}
-                },
-            }
-        ]
-    };
+  const spec = {
+    '$schema': 'https://vega.github.io/schema/vega-lite/v2.json',
+    'width': 300,
+    'height': 300,
+    'data': {'values': values},
+    'layer': [
+      {
+        'mark': 'point',
+        'encoding': {
+          'x': {'field': 'x', 'type': 'quantitative'},
+          'y': {'field': 'y', 'type': 'quantitative'}
+        }
+      },
+      {
+        'mark': 'line',
+        'encoding': {
+          'x': {'field': 'x', 'type': 'quantitative'},
+          'y': {'field': 'pred', 'type': 'quantitative'},
+          'color': {'value': 'tomato'}
+        },
+      }
+    ]
+  };
 
-    return renderChart(container, spec, {actions: false});
+  return renderChart(container, spec, {actions: false});
 }
