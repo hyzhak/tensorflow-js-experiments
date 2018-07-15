c3b77b2957defad018ec6deab40d07bf42dea905
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-07-16 00:48:24 +0200

    remove obsolete code

--- src/ui/model-graphics.js
+++ src/ui/model-graphics.js
@@ -1,40 +1,14 @@
 import vegaEmbed from 'vega-embed';
 
-const modelGraphicSpec = {
-  "$schema": "https://vega.github.io/schema/vega-lite/v2.json",
-  "width": 400,
-  "title": "model",
-  "layer": [{
-    "data": {
-      "name": "dataset",
-    },
-    "mark": "circle",
-    "encoding": {
-      "x": {"field": "x", "type": "quantitative"},
-      "y": {"field": "y", "type": "quantitative", "scale": {"domain": [0.0, 1.0]}},
-      "title": "dataset",
-    }
-  }, {
-    "data": {
-      "name": "approximation",
-    },
-    "mark": "line",
-    "encoding": {
-      "x": {"field": "x", "type": "quantitative"},
-      "y": {"field": "y", "type": "quantitative", "scale": {"domain": [0.0, 1.0]}},
-      "title": "approximation",
-    }
-  }],
-};
-
 
 /**
  * Build model graphics
  *
  * @param containerId
+ * @param values
  * @returns {Promise.<*>}
  */
-export async function build(containerId, dataSetValues) {
+export async function build(containerId, values) {
   const containerEl = document.getElementById(containerId);
   if (!containerEl) {
     throw new Error(`Model container ${containerId} is not defined`);
@@ -46,7 +20,7 @@ export async function build(containerId, dataSetValues) {
     "layer": [{
       "data": {
         "name": "dataset",
-        "values": dataSetValues,
+        "values": values,
       },
       "mark": "circle",
       "encoding": {
