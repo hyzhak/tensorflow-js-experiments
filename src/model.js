aef14e72dfe85ba5a12eda1d452fa4d2d99fb883
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-07-16 00:18:28 +0200

    remove obsolete code

--- src/model.js
+++ src/model.js
@@ -2,91 +2,11 @@ import './main.scss';
 
 import {zip} from 'lodash';
 import * as tf from '@tensorflow/tfjs';
-
 import * as vega from 'vega';
 import vegaEmbed from 'vega-embed';
 
 import {polynomialRegression} from './models/polynomial-regression';
-import * as ui from './ui';
 
-async function singleUnitExample() {
-  // Define a model for linear regression.
-  const model = tf.sequential();
-  model.add(tf.layers.dense({units: 1, inputShape: [1]}));
-
-  // Prepare the model for training: Specify the loss and the optimizer.
-  model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
-
-  // Generate some synthetic data for training.
-  const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
-  const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
-
-  // Train the model using the data.
-  await model.fit(xs, ys);
-
-  // Use the model to do inference on a data point the model hasn't seen before:
-  model.predict(tf.tensor2d([5], [1, 1])).print();
-}
-
-// singleUnitExample();
-
-async function dynamicGraphic() {
-  // wait until DOM will be rendered
-  await new Promise((resolve) => setTimeout(resolve, 100));
-
-  // draw graphic
-  const parentEl = document.getElementById('graphics');
-
-  var vlSpec = {
-    '$schema': 'https://vega.github.io/schema/vega-lite/v2.json',
-    'data': {'name': 'table'},
-    'width': 400,
-    'mark': 'line',
-    'encoding': {
-      'x': {'field': 'x', 'type': 'quantitative', 'scale': {'zero': false}},
-      'y': {'field': 'y', 'type': 'quantitative'},
-      'color': {'field': 'category', 'type': 'nominal'}
-    }
-  };
-
-  const res = await vegaEmbed(parentEl, vlSpec, {
-    actions: false,
-  });
-
-  /**
-   * Generates a new tuple with random walk.
-   */
-  function seriesGenerator() {
-    var counter = -1;
-    var previousY = [0, 0, 0, 0];
-    return () => {
-      counter++;
-      var newVals = previousY.map((v, c) => ({
-        x: counter,
-        y: v + Math.random() * c - c / 2,
-        category: c
-      }));
-      previousY = newVals.map((v) => v.y);
-      return newVals;
-    };
-  }
-
-  // generate data on-fly
-  var valueGenerator = seriesGenerator();
-  var minimumX = -100;
-
-  window.setInterval(() => {
-    minimumX++;
-
-    const changeSet = vega.changeset()
-      .insert(valueGenerator())
-      .remove((t) => t.x < minimumX);
-
-    res.view
-      .change('table', changeSet)
-      .run();
-  }, 1000 / 30);
-}
 
 const lossGraphicSpec = {
   "$schema": "https://vega.github.io/schema/vega-lite/v2.json",
@@ -100,13 +20,13 @@ const lossGraphicSpec = {
     "x": {
       "field": "x",
       "type": "quantitative",
-      "scale": {"domain": [0.0,80.0]},
+      "scale": {"domain": [0.0, 80.0]},
       "axis": {"title": "iteration"},
     },
     "y": {
       "field": "y",
       "type": "quantitative",
-      "scale": {"domain": [0.0,0.25]},
+      "scale": {"domain": [0.0, 0.25]},
       "axis": {"title": "loss"},
     },
   }
@@ -123,7 +43,7 @@ const modelGraphicSpec = {
     "mark": "circle",
     "encoding": {
       "x": {"field": "x", "type": "quantitative"},
-      "y": {"field": "y", "type": "quantitative", "scale": {"domain": [0.0,1.0]}},
+      "y": {"field": "y", "type": "quantitative", "scale": {"domain": [0.0, 1.0]}},
       "title": "dataset",
     }
   }, {
@@ -133,7 +53,7 @@ const modelGraphicSpec = {
     "mark": "line",
     "encoding": {
       "x": {"field": "x", "type": "quantitative"},
-      "y": {"field": "y", "type": "quantitative", "scale": {"domain": [0.0,1.0]}},
+      "y": {"field": "y", "type": "quantitative", "scale": {"domain": [0.0, 1.0]}},
       "title": "approximation",
     }
   }],
